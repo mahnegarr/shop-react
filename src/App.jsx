fac13252d5a75f50c0a54d88855680361e2af016
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-03-09 12:11:28 +0330

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import { Navigate, Route, Router, Routes } from 'react-router-dom';
-import ProductsPage from './ProductsPage'
-
-
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./ProductsPage";
+import DetailsPage from "./DetailsPage";
+import CheckoutPage from "./CheckoutPage";
+import PageNotFound from "./404";
 
 function App() {
-
-
   return (
     <Routes>
       <Route path="/" to={<Navigate to="/products" replace />} />
-      <Route path='/products' element={<ProductsPage />} />
-      <Route path='/products/:id' element={<ProductsPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;

d371abc88d99b7422f8fa711dd41947747a3ece0
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-03-09 12:06:20 +0330

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { Navigate, Route, Router, Routes } from 'react-router-dom';
+import ProductsPage from './ProductsPage'
+
 
 
 function App() {
 
 
   return (
-    <>
-   <div>hi</div>
-    </>
+    <Routes>
+      <Route path="/" to={<Navigate to="/products" replace />} />
+      <Route path='/products' element={<ProductsPage />} />
+      <Route path='/products/:id' element={<ProductsPage />} />
+    </Routes>
   )
 }
 

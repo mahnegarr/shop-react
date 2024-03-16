6cd36a65eb79970f1538e7732b168c56ac6c222e
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-03-16 17:04:08 +0330

    #

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import ProductsPage from "./ProductsPage";
 import DetailsPage from "./DetailsPage";
 import CheckoutPage from "./CheckoutPage";
 import PageNotFound from "./404";
+import ProductsProvider from "./context/ProductsContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" to={<Navigate to="/products" replace />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<DetailsPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route path="/" to={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </ProductsProvider>
   );
 }
 
---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,32 @@
+import api from "../services/config";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const ProductsContext = createContext();
+function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <ProductsContext.Provider value={products}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
+
+const useProducts = () => {
+  const products = useContext(ProductsContext);
+  return products;
+};
+
+export default ProductsProvider;
+export { useProducts };

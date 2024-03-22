75e41c5f0528a1005f280707a267ded42f8314bc
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-03-22 22:47:36 +0330

    #

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,22 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 import { useProducts } from "./context/ProductsContext";
 import Card from "./components/Card";
 import Loader from "./components/Loader";
+import { useEffect } from "react";
 
 function ProductsPage() {
   const products = useProducts();
 
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }));
+  };
+
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+
+    if (tagName !== "LI") return;
+    setQuery((query) => ({ ...query, category }));
+  };
+
   return (
-    <div className="flex justify-between">
-      <div className="w-full flex flex-wrap justify-between">
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <p className="bg-white">
-            <Card key={p.id} data={p} />
-          </p>
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-    </div>
+      <div className="flex justify-between">
+        <div className="w-full flex flex-wrap justify-between">
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <p className="bg-white">
+              <Card key={p.id} data={p} />
+            </p>
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelry</li>
+            <li>Men's Clothing</li>
+            <li>women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
